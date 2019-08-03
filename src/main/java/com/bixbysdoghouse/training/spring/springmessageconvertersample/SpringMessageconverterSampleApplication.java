6fd77d562de6b0c284d89e7ba54a0c322297fe44
DaveCarlson <davicar319@acm.org>
2019-08-03 10:46:10 -0500

    Initial commit.

---
+++ src/main/java/com/bixbysdoghouse/training/spring/springmessageconvertersample/SpringMessageconverterSampleApplication.java
@@ -0,0 +1,13 @@
+package com.bixbysdoghouse.training.spring.springmessageconvertersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessageconverterSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMessageconverterSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bixbysdoghouse/training/spring/springmessageconvertersample/SpringMessageconverterSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bixbysdoghouse.training.spring.springmessageconvertersample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMessageconverterSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
