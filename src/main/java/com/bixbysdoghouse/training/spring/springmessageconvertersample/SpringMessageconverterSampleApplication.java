4b91ef651899855b819a495bf95b81c5d90d6071
DaveCarlson <davicar319@acm.org>
2019-08-03 11:14:05 -0500

    Adding copyright information.

--- src/main/java/com/bixbysdoghouse/training/spring/springmessageconvertersample/SpringMessageconverterSampleApplication.java
+++ src/main/java/com/bixbysdoghouse/training/spring/springmessageconvertersample/SpringMessageconverterSampleApplication.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2019 David A Carlson, Bixby's Dog House Software.
+ *
+ * Free to use and/or modify under the Apache License 2.0.  See the LICENSE file in the root directory
+ *  of the project for more information.
+ *                                  Apache License
+ *                            Version 2.0, January 2004
+ *                         http://www.apache.org/licenses/
+ *
+ */
+
 package com.bixbysdoghouse.training.spring.springmessageconvertersample;
 
 import org.springframework.boot.SpringApplication;
