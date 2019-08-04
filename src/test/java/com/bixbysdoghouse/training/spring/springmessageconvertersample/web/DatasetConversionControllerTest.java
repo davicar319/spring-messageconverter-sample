aa6c5adb4d229bd622dfcd56f5d2171bc88825dc
DaveCarlson <davicar319@acm.org>
2019-08-04 14:20:17 -0500

    Getting Jena in as a dependency.

--- src/test/java/com/bixbysdoghouse/training/spring/springmessageconvertersample/web/DatasetConversionControllerTest.java
+++ src/test/java/com/bixbysdoghouse/training/spring/springmessageconvertersample/web/DatasetConversionControllerTest.java
@@ -16,13 +16,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(DatasetConversionController.class)
+@WebMvcTest(DatasetConversionControllerTest.class)
 public class DatasetConversionControllerTest {
 
+    private static final MediaType JSON_LD_MEDIA_TYPE = MediaType.valueOf("application/ld+json;charset=UTF-8");
+    private static final MediaType NQUADS_MEDIA_TYPE = MediaType.valueOf("application/n-quads");
     @Autowired
     MockMvc mockMvc;
 
@@ -30,4 +35,21 @@ public class DatasetConversionControllerTest {
     public void canary() {
         Assert.assertNotNull(mockMvc);
     }
+
+    @Test
+    public void nQuadsCanBeConvertedToJsonLd() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/rdf/converter")
+                .contentType(NQUADS_MEDIA_TYPE)
+                .accept(JSON_LD_MEDIA_TYPE)
+                .content(SIMPLE_NQUADS))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().contentType(JSON_LD_MEDIA_TYPE));
+    }
+    private static String getExpectedSimpleJsonLd() {
+    return "";
+    }
+    private static final String SIMPLE_NQUADS = "<http://one.example/subject1> <http://one.example/predicate1> <http://one.example/object1> <http://example.org/graph3> .";
+
+
 }
